c95216baa81de0072c6b1735bca1219121a03481
DOCtorActoAntohich <fairy666death@mail.ru>
2022-04-26 21:52:26 +0300

    Add more generation settings

--- CubeSurferClone/Assets/Scripts/CubeSurfer/EcsComponent/Level/GenerationSettings.cs
+++ CubeSurferClone/Assets/Scripts/CubeSurfer/EcsComponent/Level/GenerationSettings.cs
@@ -1,11 +1,22 @@
 using System;
+using UnityEngine;
 
 namespace CubeSurfer.EcsComponent.Level
 {
     [Serializable]
     public struct GenerationSettings
     {
-        public bool lavaLakes;
+        [Header("Features")]
+        public bool walls;
         public bool turns;
+        public bool lavaLakes;
+
+        [Range(5, 25)]
+        public int platformsAmount;
+
+        [Header("Sizes")]
+        public float platformWidth;
+        public float platformLength;
+        public float platformHeight;
     }
 }
