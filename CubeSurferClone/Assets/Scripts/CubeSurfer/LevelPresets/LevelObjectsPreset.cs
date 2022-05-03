a2bbb2a49f512f4940b2bf865753d7f2d8b5612b
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-03 14:55:52 +0300

    Add more walls patterns

---
+++ CubeSurferClone/Assets/Scripts/CubeSurfer/LevelPresets/LevelObjectsPreset.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace CubeSurfer.LevelPresets
+{
+    [CreateAssetMenu(fileName = "New Level Objects Preset", menuName = "Level Objects Preset")]
+    public class LevelObjectsPreset : ScriptableObject
+    {
+        public GameObject startPlatform;
+        public GameObject standardPlatform;
+        
+        public GameObject turnLeftPlatform;
+        public GameObject turnRightPlatform;
+
+        public GameObject[] wallsPlatforms;
+
+        public GameObject[] bonusPlatforms;
+
+        public GameObject[] lavaPlatforms;
+    }
+}
